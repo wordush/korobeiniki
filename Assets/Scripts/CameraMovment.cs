d6369bfc43bb4ba6f4e92fb62f4f2bf7f175ef3d
P4elme6ka <006gta1@gmail.com>
2019-09-20 11:42:52 +0300

    много всего update

--- Assets/Scripts/CameraMovment.cs
+++ Assets/Scripts/CameraMovment.cs
@@ -16,7 +16,8 @@ public class CameraMovment : MonoBehaviour
     private Quaternion camRot;
     private GameObject camObj;
     public Vector3 CameraPositionByHit;
-    
+    private float angle;
+
     private RaycastHit hit;
     private Vector3 targetPos;    
     
@@ -42,7 +43,10 @@ public class CameraMovment : MonoBehaviour
         switch (mode)
         {
             case CameraMode.AdaptiveHightMode:
-                Physics.Raycast(new Ray(targetPos, Vector3.down), out hit);
+                Ray ray = new Ray(targetPos, Vector3.down);
+                int layerMask = 1 << 8;
+                layerMask = ~layerMask;
+                Physics.Raycast(ray, out hit,Mathf.Infinity,layerMask);
 
                 camRot = Quaternion.LookRotation(hit.point - camPos, Vector3.up);
 
@@ -50,8 +54,8 @@ public class CameraMovment : MonoBehaviour
                 Scale = Mathf.Clamp(Scale,minScale,maxScale);
                 camPos = hit.point + CameraPositionByHit * Scale;
 
-                camObj.transform.position = Vector3.Lerp(camObj.transform.position, camPos , Time.deltaTime * 3);
-                camObj.transform.rotation = Quaternion.Lerp(camObj.transform.rotation,camRot, Time.deltaTime * 3);
+                camObj.transform.position = Vector3.Lerp(camObj.transform.position, camPos , Time.deltaTime * 10);
+                camObj.transform.rotation = Quaternion.Lerp(camObj.transform.rotation,camRot, Time.deltaTime * 10);
                 break;
             case CameraMode.FixedHeightMode:
                 Debug.LogError("Я это еще не доделал!!!, переключи mode на AdaptiveHightMode");
@@ -63,21 +67,47 @@ public class CameraMovment : MonoBehaviour
     {
         if ((Input.mousePosition.y < moveSidesSize && Input.mousePosition.y > 0) || (Input.GetKey(KeyCode.S)))// Down
         {
-            targetPos.z -= slideSensitivity * Scale * 0.1f;
+            targetPos.z -= Mathf.Cos(angle * Mathf.Deg2Rad) * slideSensitivity * Scale * 0.1f;
+            targetPos.x -= Mathf.Sin(angle * Mathf.Deg2Rad) * slideSensitivity * Scale * 0.1f;
         }
         else
         if ((Screen.height - Input.mousePosition.y < moveSidesSize && Input.mousePosition.y < Screen.height) || (Input.GetKey(KeyCode.W)))// Up
         {
-            targetPos.z += slideSensitivity * Scale * 0.1f;
+            targetPos.z += Mathf.Cos(angle * Mathf.Deg2Rad) * slideSensitivity * Scale * 0.1f;
+            targetPos.x += Mathf.Sin(angle * Mathf.Deg2Rad) * slideSensitivity * Scale * 0.1f;
         }
         if ((Input.mousePosition.x < moveSidesSize && Input.mousePosition.x > 0) || (Input.GetKey(KeyCode.A))) // Left 
         {
-            targetPos.x -= slideSensitivity * Scale * 0.1f;
+            targetPos.z += Mathf.Sin(angle * Mathf.Deg2Rad) * slideSensitivity * Scale * 0.1f;
+            targetPos.x -= Mathf.Cos(angle * Mathf.Deg2Rad) * slideSensitivity * Scale * 0.1f;
         }
         else
         if ((Screen.width - Input.mousePosition.x < moveSidesSize && Input.mousePosition.x < Screen.width) || (Input.GetKey(KeyCode.D)))// Right
         {
-            targetPos.x += slideSensitivity * Scale * 0.1f;
+            targetPos.z -= Mathf.Sin(angle * Mathf.Deg2Rad) * slideSensitivity * Scale * 0.1f;
+            targetPos.x += Mathf.Cos(angle * Mathf.Deg2Rad) * slideSensitivity * Scale * 0.1f;
         }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            angle += 180 * Time.deltaTime;
+            camPos = RotateAround(camPos,new Vector3(hit.point.x,0, hit.point.z),180 * Time.deltaTime);
+            CameraPositionByHit = camPos - hit.point;
+        }
+        else
+        if (Input.GetKey(KeyCode.E))
+        {
+            angle -= 180 * Time.deltaTime;
+            camPos = RotateAround(camPos, new Vector3(hit.point.x, 0, hit.point.z), -180 * Time.deltaTime);
+            CameraPositionByHit = camPos - hit.point;
+        }
+
+    }
+
+    public Vector3 RotateAround(Vector3 pos,Vector3 point,float angle)
+    {
+        Vector3 dir = pos - point;
+        dir = Quaternion.Euler(new Vector3(0, angle, 0)) * dir;
+        pos = dir + point;
+        return pos;
     }
 }
--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -5,62 +5,72 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using System;
 
-[Serializable]
-public class GameData
+namespace GameStructure
 {
-    int ImpratorLoyality;
-    int Money;
-    int PeasantsLoyality;
-    int Peasants;
-    GameStatment Satatment;
-    GameProgress Progress;
-
-    public static void SaveGameData(GameData data, string name)
+    public enum Level
     {
-        name += ".korobeinikiData";
-        if (File.Exists(name))
+        Level1,
+        Level2,
+        Level3,
+        Level4,
+        Level5
+    }
+
+    [Serializable]
+    public class GameData
+    {
+        int ImpratorLoyality;
+        int Money;
+        int PeasantsLoyality;
+        int Peasants;
+        GameStatment Satatment;
+        GameProgress Progress;
+
+        public static void SaveGameData(GameData data, string name)
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + name, FileMode.Open);
-            bf.Serialize(file,data);
-            file.Close();
+            name += ".korobeinikiData";
+            if (File.Exists(name))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(Application.persistentDataPath + name, FileMode.Open);
+                bf.Serialize(file, data);
+                file.Close();
+            }
+            else
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(Application.persistentDataPath + name, FileMode.Create);
+                bf.Serialize(file, data);
+                file.Close();
+            }
         }
-        else
+
+        public static GameData LoadGameData(string name)
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + name, FileMode.Create);
-            bf.Serialize(file, data);
-            file.Close();
+            name += ".korobeinikiData";
+            if (File.Exists(name))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(Application.persistentDataPath + name, FileMode.Open);
+                GameData data = (GameData)bf.Deserialize(file);
+                file.Close();
+                return data;
+            }
+            else
+            {
+                Debug.LogError("File " + Application.persistentDataPath + name + " does not exists");
+                return null;
+            }
         }
     }
 
-    public static GameData LoadGameData(string name)
+    public class GameStatment
     {
-        name += ".korobeinikiData";
-        if (File.Exists(name))
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + name, FileMode.Open);
-            GameData data = (GameData)bf.Deserialize(file);
-            file.Close();
-            return data;
-        }else
-        {
-            Debug.LogError("File " + Application.persistentDataPath + name +" does not exists");
-            return null;
-        }
+        // buildings, enviroment
     }
 
+    public class GameProgress
+    {
 
-
-}
-
-public class GameStatment
-{
-    // buildings, enviroment
-}
-
-public class GameProgress
-{
-    //missions, progress in this missions
+    }
 }
