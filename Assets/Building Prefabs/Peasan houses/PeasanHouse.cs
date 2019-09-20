4392368ee52cd9a16b876c97f3f070771db582ce
P4elme6ka <006gta1@gmail.com>
2019-09-20 11:45:03 +0300

    Building Prefabs

---
+++ Assets/Building Prefabs/Peasan houses/PeasanHouse.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameStructure;
+using UnityEngine.Events;
+
+public class PeasanHouse : MonoBehaviour
+{
+    public GameObject[] houseObects;
+    public ObjectInformation information;
+
+    float BuildigTime = 10;
+    int Capacity;
+    public Level level;
+
+
+
+    void Start()
+    {
+        information = GetComponent<ObjectInformation>();
+        level = Level.Level1;
+        UpdateValues(level);
+        information.Functions.Add(LevelUp);
+        information.Functions.Add(DeleteThisSheet);
+    }
+
+    public GameObject GetActiveHouse()
+    {
+        return houseObects[(int)level];
+    }
+
+    public void DeleteThisSheet()
+    {
+        SelectedObject.Deselect();
+        Destroy(gameObject,1);
+    }
+
+    public void LevelUp()
+    {
+        if (level < Level.Level5)
+        { 
+            level += 1;
+            UpdateValues(level);
+        }
+    }
+
+    public void UpdateValues(Level lev)
+    {
+        information.LinkDown = houseObects[(int)level].GetComponent<ObjectInformation>();
+        foreach (GameObject obj in houseObects)
+        {
+            obj.SetActive(false);
+        }
+
+        switch (lev)
+        {
+            case Level.Level1:
+                houseObects[0].SetActive(true);
+                break;
+            case Level.Level2:
+                houseObects[1].SetActive(true);
+                break;
+            case Level.Level3:
+                houseObects[2].SetActive(true);
+                break;
+            case Level.Level4:
+                houseObects[3].SetActive(true);
+                break;
+            case Level.Level5:
+                houseObects[4].SetActive(true);
+                break;
+            default:
+                houseObects[0].SetActive(true);
+                break;
+        }
+    }
+}
---
+++ Assets/Building Prefabs/Sawmill/Sawmill.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameStructure;
+
+public class Sawmill : MonoBehaviour
+{
+    public float store;
+    public float capacity;
+    public ObjectInformation information;
+    public int perfomance;
+    public Level level;
+
+    public void Start()
+    {
+        information = GetComponent<ObjectInformation>();
+    }
+
+    public void Update()
+    {
+        store += Mathf.Clamp(perfomance * Time.deltaTime,0,capacity);
+        information.Description = ((int)store).ToString() + ": wood in the bank";
+        if(SelectedObject.IsActiv() && SelectedObject.Get().Equals(gameObject))
+            GameObject.Find("ObjectMenu").GetComponent<ObjectMenuHandler>().OnInformationChanged();
+    }
+
+    public void LevelUp()
+    {
+        if (level < Level.Level5)
+        {
+            level += 1;
+            UpdateValues(level);
+        }
+    }
+
+    public void UpdateValues(Level lev)
+    {
+        switch (lev)
+        {
+            case Level.Level1:
+
+                break;
+            case Level.Level2:
+
+                break;
+            case Level.Level3:
+
+                break;
+            case Level.Level4:
+
+                break;
+            case Level.Level5:
+
+                break;
+            default:
+
+                break;
+        }
+    }
+
+
+}
